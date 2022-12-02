ed45fcd6a38d8405d3d6f0e728b40879c4f7db28
C-K-Loan <christian.kasim.loan@gmail.com>
2022-12-02 18:54:32 +0100

    [BUMP] upgrade spark-nlp, enterprise-nlp, visual versions

--- johnsnowlabs/auto_install/lib_resolvers/nlp_installer.py
+++ johnsnowlabs/auto_install/lib_resolvers/nlp_installer.py
@@ -47,17 +47,20 @@ class NlpLibResolver(Py4JJslLibDependencyResolverABC, metaclass=ABCMeta):
     }
 
     compatible_spark_to_py_map = {
+
+        # https://files.pythonhosted.org/packages/2c/56/29a8663d25daceb1b2ac18f292725295cace90361a96453b432d00bc5560/spark_nlp-4.2.4-py2.py3-none-any.whl
+        # https://files.pythonhosted.org/packages/b1/ab/349d8f0ecbf431046470827c065d0a045d113d6dae166a1b6c576b0ad21b/spark-nlp-4.2.4.tar.gz
         SparkVersion.spark3xx: {
             # TODO HARDCODE HASH!!! OR grap from enum or somwhere comfy. Maybe configs/settings file?
             PyInstallTypes.wheel: UrlDependency(
-                url='https://files.pythonhosted.org/packages/6b/5f/e321241e4c7a1c53bfe7792dbd5a5622d265142aedd088f9ce5a0f67c2a4/spark_nlp-{lib_version}-py2.py3-none-any.whl',
+                url='https://files.pythonhosted.org/packages/2c/56/29a8663d25daceb1b2ac18f292725295cace90361a96453b432d00bc5560/spark_nlp-{lib_version}-py2.py3-none-any.whl',
                 dependency_type=PyInstallTypes.wheel,
                 spark_version=SparkVersion.spark3xx,
                 product_name=product_name,
                 file_name=product_name.name,
                 dependency_version=lib_version),
             PyInstallTypes.tar: UrlDependency(
-                url='https://files.pythonhosted.org/packages/78/7e/1ed94f903c0dfe0e6d4900bf61d0210cb39dadf918c7a21f9cfdf924fc50/park-nlp-{lib_version}.tar.gz',
+                url='https://files.pythonhosted.org/packages/b1/ab/349d8f0ecbf431046470827c065d0a045d113d6dae166a1b6c576b0ad21b/spark-nlp-{lib_version}.tar.gz',
                 dependency_type=PyInstallTypes.tar,
                 spark_version=SparkVersion.spark3xx,
                 product_name=product_name,
