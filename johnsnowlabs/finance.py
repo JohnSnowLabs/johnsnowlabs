0068da885ffbcb607099e4e6c215f5de71fdfcad
C-K-Loan <christian.kasim.loan@gmail.com>
2023-01-21 08:28:22 +0100

    [FEATURE] DocMapper,NameObfuscator,DocMlClassifier,Resolution2Chunk,ContextParserModel for finance/legal/medical

--- johnsnowlabs/finance.py
+++ johnsnowlabs/finance.py
@@ -1,89 +1,101 @@
+import traceback
 
 from johnsnowlabs.abstract_base.lib_resolver import try_import_lib
 from johnsnowlabs.utils.print_messages import log_broken_lib
 
 try:
 
-    if try_import_lib('sparknlp_jsl') and try_import_lib('sparknlp'):
-        # Substitutions
-        from sparknlp_jsl.finance import FinanceBertForTokenClassification as BertForTokenClassification
-        from sparknlp_jsl.finance import FinanceNerApproach as NerApproach
-        from sparknlp_jsl.finance import FinanceNerModel as NerModel
-        from sparknlp_jsl.finance import FinanceBertForSequenceClassification as BertForSequenceClassification
-        from sparknlp_jsl.finance import FinanceBertForSequenceClassification as BertForSequenceClassification
-        from sparknlp_jsl.finance import FinanceClassifierDLApproach as ClassifierDLApproach
-        from sparknlp_jsl.finance import FinanceClassifierDLModel as ClassifierDLModel
-        # from sparknlp_jsl.finance import FinanceDocumentHashCoder as DocumentHashCoder
-        # from sparknlp_jsl.finance import FinanceNerQuestionGenerator as QuestionGenerator
-        from sparknlp_jsl.finance.chunk_classification.deid.document_hashcoder import FinanceDocumentHashCoder as DocumentHashCoder
-        from sparknlp_jsl.finance.seq_generation.qa_ner_generator import FinanceNerQuestionGenerator as NerQuestionGenerator
-        from sparknlp_jsl.annotator import AssertionChunkConverter
-        from sparknlp_jsl.finance import SentenceEntityResolverModel, \
-            ChunkMapperModel, \
-            AssertionDLModel, \
-            RelationExtractionDLModel, \
-            ZeroShotRelationExtractionModel, \
-            ChunkMapperApproach, \
-            SentenceEntityResolverApproach, \
-            AssertionDLApproach, \
-            ZeroShotNerModel
-
-        from sparknlp_jsl import training_log_parser
-
-        from sparknlp_jsl.annotator import MedicalDistilBertForSequenceClassification as DistilBertForSequenceClassification, \
-    AssertionChunkConverter
-
-        # These are licensed annos shared across all libs
-        from sparknlp_jsl.annotator import \
-            AssertionLogRegModel, \
-            DeIdentificationModel, \
-            DocumentLogRegClassifierModel, \
-            RelationExtractionModel, \
-            ChunkMergeModel, \
-            BertSentenceChunkEmbeddings, \
-            ChunkKeyPhraseExtraction, \
-            NerDisambiguatorModel, \
-            EntityChunkEmbeddings, \
-            TFGraphBuilder, \
-            ChunkConverter, \
-            ChunkFilterer, \
-            NerConverterInternal, \
-            NerChunker, \
-            AssertionFilterer, \
-            AnnotationMerger, \
-            RENerChunksFilter, \
-            ChunkSentenceSplitter, \
-            ChunkMapperFilterer, \
-            DateNormalizer, \
-            GenericClassifierModel, \
-            ReIdentification
-
+    if try_import_lib("sparknlp_jsl") and try_import_lib("sparknlp"):
         from sparknlp_jsl.structured_deidentification import StructuredDeidentification
-        from sparknlp_jsl.annotator.resolution.resolver_merger import ResolverMerger
-
         from sparknlp_jsl.base import FeaturesAssembler
 
-        from sparknlp_jsl.annotator import \
-            AssertionLogRegApproach, \
-            DeIdentification, \
-            DocumentLogRegClassifierApproach, \
-            RelationExtractionApproach, \
-            ChunkMergeApproach, \
-            NerDisambiguator, \
-            ContextualParserApproach, \
-            GenericClassifierApproach, \
-            Router, \
-            NerQuestionGenerator, \
-            DocumentHashCoder, \
-            Replacer
+        from sparknlp_jsl.finance import (
+            FinanceNerQuestionGenerator as NerQuestionGenerator,
+            FinanceDocumentHashCoder as DocumentHashCoder,
+            FinanceBertForTokenClassification as BertForTokenClassification,
+            FinanceDocumentMLClassifierModel as DocumentMLClassifierModel,
+            FinanceDocumentMLClassifierApproach as DocumentMLClassifierApproach,
+            DocMapperApproach,
+            DocMapperModel,
+            FinanceNerApproach as NerApproach,
+            FinanceNerModel as NerModel,
+            FinanceClassifierDLModel as ClassifierDLModel,
+            FinanceBertForSequenceClassification as BertForSequenceClassification,
+            FinanceBertForSequenceClassification as BertForSequenceClassification,
+            FinanceClassifierDLApproach as ClassifierDLApproach,
+            DocMapperModel,
+            DocMapperApproach,
+            SentenceEntityResolverModel,
+            ChunkMapperModel,
+            AssertionDLModel,
+            RelationExtractionDLModel,
+            ZeroShotRelationExtractionModel,
+            ChunkMapperApproach,
+            SentenceEntityResolverApproach,
+            AssertionDLApproach,
+            ZeroShotNerModel,
+        )
+
+        # These are licensed annos shared across all libs
+        from sparknlp_jsl.annotator import (
+            MedicalDistilBertForSequenceClassification as DistilBertForSequenceClassification,
+            AssertionChunkConverter,
+            AssertionLogRegModel,
+            DeIdentificationModel,
+            DocumentLogRegClassifierModel,
+            RelationExtractionModel,
+            ChunkMergeModel,
+            BertSentenceChunkEmbeddings,
+            ChunkKeyPhraseExtraction,
+            NerDisambiguatorModel,
+            EntityChunkEmbeddings,
+            TFGraphBuilder,
+            ChunkConverter,
+            ChunkFilterer,
+            NerConverterInternal,
+            NerChunker,
+            AssertionFilterer,
+            AnnotationMerger,
+            RENerChunksFilter,
+            ChunkSentenceSplitter,
+            ChunkMapperFilterer,
+            DateNormalizer,
+            GenericClassifierModel,
+            ReIdentification,
+            AssertionLogRegApproach,
+            DeIdentification,
+            DocumentLogRegClassifierApproach,
+            RelationExtractionApproach,
+            ChunkMergeApproach,
+            NerDisambiguator,
+            ContextualParserApproach,
+            GenericClassifierApproach,
+            Router,
+            NerQuestionGenerator,
+            Replacer,
+            # NEW
+            ContextualParserModel,
+            NameChunkObfuscatorApproach,
+            NameChunkObfuscator,
+            Resolution2Chunk,
+            ResolverMerger,
+        )
+
         from sparknlp_jsl.modelTracer import ModelTracer
-
-
+        from sparknlp_jsl import training_log_parser
         from sparknlp_jsl.compatibility import Compatibility
         from sparknlp_jsl.pretrained import InternalResourceDownloader
-        from sparknlp_jsl.eval import NerDLMetrics, NerDLEvaluation, SymSpellEvaluation, POSEvaluation, \
-            NerCrfEvaluation, NorvigSpellEvaluation
+        from sparknlp_jsl.eval import (
+            NerDLMetrics,
+            NerDLEvaluation,
+            SymSpellEvaluation,
+            POSEvaluation,
+            NerCrfEvaluation,
+            NorvigSpellEvaluation,
+        )
 
-except:
-    if try_import_lib('sparknlp_jsl') and try_import_lib('sparknlp'):
-        log_broken_lib('Enterprise Finance')
+except Exception as err:
+    if try_import_lib("sparknlp_jsl") and try_import_lib("sparknlp"):
+        log_broken_lib("Enterprise Finance")
+        print(f"Error Message : {err}")
+        print(f"Error Trace: {traceback.format_exc()}")
--- johnsnowlabs/legal.py
+++ johnsnowlabs/legal.py
@@ -1,89 +1,101 @@
+import traceback
+
 from johnsnowlabs.abstract_base.lib_resolver import try_import_lib
 from johnsnowlabs.utils.print_messages import log_broken_lib
 
 try:
-    if try_import_lib('sparknlp_jsl') and try_import_lib('sparknlp'):
-        # Substitutions
-        from sparknlp_jsl.legal import LegalBertForTokenClassification as BertForTokenClassification
-        from sparknlp_jsl.legal import LegalNerApproach as NerApproach
-        from sparknlp_jsl.legal import LegalNerModel as NerModel
-        from sparknlp_jsl.legal import LegalBertForSequenceClassification as BertForSequenceClassification
-        from sparknlp_jsl.legal import LegalBertForSequenceClassification as BertForSequenceClassification
-        from sparknlp_jsl.legal import LegalClassifierDLApproach as ClassifierDLApproach
-        from sparknlp_jsl.legal import LegalClassifierDLModel as ClassifierDLModel
-        from sparknlp_jsl.annotator import \
-            MedicalDistilBertForSequenceClassification as DistilBertForSequenceClassification
-        # from sparknlp_jsl.legal import LegalDocumentHashCoder as DocumentHashCoder
-        # from sparknlp_jsl.legal import LegalNerQuestionGenerator as QuestionGenerator
-        from sparknlp_jsl.legal.chunk_classification.deid.document_hashcoder import \
-            LegalDocumentHashCoder as DocumentHashCoder
-        from sparknlp_jsl.legal.seq_generation.qa_ner_generator import LegalNerQuestionGenerator as NerQuestionGenerator
-        from sparknlp_jsl.annotator import AssertionChunkConverter
-
-        from sparknlp_jsl.legal import SentenceEntityResolverModel, \
-            ChunkMapperModel, \
-            AssertionDLModel, \
-            RelationExtractionDLModel, \
-            ZeroShotRelationExtractionModel, \
-            ChunkMapperApproach, \
-            SentenceEntityResolverApproach, \
-            AssertionDLApproach, \
-            ZeroShotNerModel
-
-        # These are licensed annos shared across all libs
-        from sparknlp_jsl.annotator import \
-            AssertionLogRegModel, \
-            DeIdentificationModel, \
-            DocumentLogRegClassifierModel, \
-            RelationExtractionModel, \
-            ChunkMergeModel, \
-            ChunkMapperModel, \
-            BertSentenceChunkEmbeddings, \
-            ChunkKeyPhraseExtraction, \
-            NerDisambiguatorModel, \
-            EntityChunkEmbeddings, \
-            TFGraphBuilder, \
-            ChunkConverter, \
-            ChunkFilterer, \
-            NerConverterInternal, \
-            NerChunker, \
-            AssertionFilterer, \
-            AnnotationMerger, \
-            RENerChunksFilter, \
-            ChunkSentenceSplitter, \
-            ChunkMapperFilterer, \
-            DateNormalizer, \
-            GenericClassifierModel, \
-            ReIdentification, \
-            Replacer
-        from sparknlp_jsl.modelTracer import ModelTracer
-
-        # DrugNormalizer, \
-        from sparknlp_jsl.structured_deidentification import StructuredDeidentification
-        from sparknlp_jsl.annotator.resolution.resolver_merger import ResolverMerger
+    if try_import_lib("sparknlp_jsl") and try_import_lib("sparknlp"):
         from sparknlp_jsl.base import FeaturesAssembler
 
-        from sparknlp_jsl.annotator import \
-            AssertionLogRegApproach, \
-            DeIdentification, \
-            DocumentLogRegClassifierApproach, \
-            RelationExtractionApproach, \
-            ChunkMergeApproach, \
-            NerDisambiguator, \
-            ContextualParserApproach, \
-            GenericClassifierApproach, \
-            Router, \
-            NerQuestionGenerator, \
-            DocumentHashCoder, \
-            AssertionChunkConverter
-        from sparknlp_jsl import training_log_parser
+        from sparknlp_jsl.legal import (
+            LegalDocumentHashCoder as DocumentHashCoder,
+            LegalNerQuestionGenerator as NerQuestionGenerator,
+            LegalBertForSequenceClassification as BertForSequenceClassification,
+            LegalDocumentMLClassifierModel as DocumentMLClassifierModel,
+            LegalDocumentMLClassifierApproach as DocumentMLClassifierApproach,
+            DocMapperModel,
+            DocMapperApproach,
+            LegalBertForSequenceClassification as BertForSequenceClassification,
+            LegalBertForTokenClassification as BertForTokenClassification,
+            LegalNerApproach as NerApproach,
+            LegalNerModel as NerModel,
+            LegalClassifierDLApproach as ClassifierDLApproach,
+            LegalClassifierDLModel as ClassifierDLModel,
+            SentenceEntityResolverModel,
+            ChunkMapperModel,
+            AssertionDLModel,
+            RelationExtractionDLModel,
+            ZeroShotRelationExtractionModel,
+            ChunkMapperApproach,
+            SentenceEntityResolverApproach,
+            AssertionDLApproach,
+            ZeroShotNerModel,
+        )
 
+        # These are licensed annos shared across all libs
+        from sparknlp_jsl.annotator import (
+            MedicalDistilBertForSequenceClassification as DistilBertForSequenceClassification,
+            AssertionChunkConverter,
+            AssertionLogRegModel,
+            DeIdentificationModel,
+            DocumentLogRegClassifierModel,
+            RelationExtractionModel,
+            ChunkMergeModel,
+            ChunkMapperModel,
+            BertSentenceChunkEmbeddings,
+            ChunkKeyPhraseExtraction,
+            NerDisambiguatorModel,
+            EntityChunkEmbeddings,
+            TFGraphBuilder,
+            ChunkConverter,
+            ChunkFilterer,
+            NerConverterInternal,
+            NerChunker,
+            AssertionFilterer,
+            AnnotationMerger,
+            RENerChunksFilter,
+            ChunkSentenceSplitter,
+            ChunkMapperFilterer,
+            DateNormalizer,
+            GenericClassifierModel,
+            ReIdentification,
+            Replacer,
+            ContextualParserModel,
+            AssertionLogRegApproach,
+            DeIdentification,
+            DocumentLogRegClassifierApproach,
+            RelationExtractionApproach,
+            ChunkMergeApproach,
+            NerDisambiguator,
+            ContextualParserApproach,
+            GenericClassifierApproach,
+            Router,
+            NerQuestionGenerator,
+            AssertionChunkConverter,
+            NameChunkObfuscatorApproach,
+            NameChunkObfuscator,
+            # Resolution2Chunk,
+            ResolverMerger,
+        )
+        from sparknlp_jsl.modelTracer import ModelTracer
+
+        from sparknlp_jsl.structured_deidentification import StructuredDeidentification
+
+        from sparknlp_jsl import training_log_parser
         from sparknlp_jsl.compatibility import Compatibility
         from sparknlp_jsl.pretrained import InternalResourceDownloader
-        from sparknlp_jsl.eval import NerDLMetrics, NerDLEvaluation, SymSpellEvaluation, POSEvaluation, \
-            NerCrfEvaluation, NorvigSpellEvaluation
+        from sparknlp_jsl.eval import (
+            NerDLMetrics,
+            NerDLEvaluation,
+            SymSpellEvaluation,
+            POSEvaluation,
+            NerCrfEvaluation,
+            NorvigSpellEvaluation,
+        )
 
 
-except:
-    if try_import_lib('sparknlp_jsl') and try_import_lib('sparknlp'):
-        log_broken_lib('Enterprise Finance')
+except Exception as err:
+    if try_import_lib("sparknlp_jsl") and try_import_lib("sparknlp"):
+        log_broken_lib("Enterprise Finance")
+        print(f"Error Message : {err}")
+        print(f"Error Trace: {traceback.format_exc()}")
