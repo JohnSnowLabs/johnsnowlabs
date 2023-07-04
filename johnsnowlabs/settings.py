49c86b47a8516cffec9b422bc99609213e0f4f21
Christian Kasim Loan <christian.kasim.loan@gmail.com>
2023-07-04 23:38:02 +0200

    bump jsl lib version and spark nlp

--- johnsnowlabs/settings.py
+++ johnsnowlabs/settings.py
@@ -9,8 +9,8 @@ from johnsnowlabs.utils.env_utils import (
 
 # These versions are used for auto-installs and version  checks
 
-raw_version_jsl_lib = "4.4.6"
-raw_version_nlp = "4.4.1"
+raw_version_jsl_lib = "4.4.9"
+raw_version_nlp = "4.4.4"
 raw_version_nlu = "4.2.2"
 
 raw_version_pyspark = "3.1.2"
