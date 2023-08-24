79e6a368d0ad8b044b71e8939a41deb2ce357374
Christian Kasim Loan <christian.kasim.loan@gmail.com>
2023-08-24 02:26:45 +0200

    bump versions

--- johnsnowlabs/settings.py
+++ johnsnowlabs/settings.py
@@ -10,20 +10,19 @@ from johnsnowlabs.utils.env_utils import (
 # These versions are used for auto-installs and version  checks
 
 
-raw_version_jsl_lib = "5.0.1"
-raw_version_nlp = "5.0.1"
+raw_version_jsl_lib = "5.0.4"
+raw_version_nlp = "5.0.2"
 
-
-raw_version_nlu = "4.2.2"
+raw_version_nlu = "5.0.0"
 
 raw_version_pyspark = "3.1.2"
 raw_version_nlp_display = "4.1"
 
-raw_version_medical = "5.0.1"
-raw_version_secret_medical = "5.0.1"
+raw_version_medical = "5.0.2"
+raw_version_secret_medical = "5.0.2"
 
-raw_version_secret_ocr = "4.4.4"
-raw_version_ocr = "4.4.4"
+raw_version_secret_ocr = "5.0.0"
+raw_version_ocr = "5.0.0"
 
 raw_version_pydantic = "1.10.11"
 
@@ -72,6 +71,7 @@ db_cluster_name = "John-Snow-Labs-Databricks-Auto-Cluster🚀"
 db_driver_node_type = "i3.xlarge"
 db_node_type_id = "i3.xlarge"
 db_spark_version = "10.5.x-scala2.12"
+# db_spark_version = "13.2.x-scala2.12"
 
 db_job_name = "John-Snow-Labs-Job {job} 🚀"
 db_run_name = "John-Snow-Labs-Run 🚀"
