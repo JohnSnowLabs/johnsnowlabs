6aa5c83fc2b181b399ca54c8d9fe015bed2caedc
Christian Kasim Loan <christian.kasim.loan@gmail.com>
2023-09-25 18:48:08 +0200

    bump versions

--- johnsnowlabs/settings.py
+++ johnsnowlabs/settings.py
@@ -9,8 +9,8 @@ from johnsnowlabs.utils.env_utils import (
 )
 
 # These versions are used for auto-installs and version  checks
-raw_version_jsl_lib = "5.0.8"
-raw_version_nlp = "5.0.2"
+raw_version_jsl_lib = "5.1.0"
+raw_version_nlp = "5.1.0"
 
 raw_version_nlu = "5.0.1"
 
@@ -18,11 +18,11 @@ raw_version_nlu = "5.0.1"
 raw_version_pyspark = "3.1.2"
 raw_version_nlp_display = "4.1"
 
-raw_version_medical = "5.0.2"
-raw_version_secret_medical = "5.0.2"
+raw_version_medical = "5.1.0"
+raw_version_secret_medical = "5.1.0"
 
-raw_version_secret_ocr = "5.0.0"
-raw_version_ocr = "5.0.0"
+raw_version_secret_ocr = "5.0.1"
+raw_version_ocr = "5.0.1"
 
 raw_version_pydantic = "1.10.11"
 
