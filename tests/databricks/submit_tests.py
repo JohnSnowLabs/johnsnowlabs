7909a7fb2b04bd6225ead4e6f521711f6ac97c9a
Christian Kasim Loan <christian.kasim.loan@gmail.com>
2023-10-06 23:48:37 +0200

    Release/513 (#690)
    * optimized EMR imports for less chance of import errors
    
    * bump versions
    
    * update release notes
    
    * drop deprecated setup.py
    
    * Endpoint tests (#637)
    
    * example databricks serve notebooks and cluster creation
    
    * updated healthcecks
    
    * databricks serve + john snow labs endpoints module
    
    * `clean_cluster` and `write_db_credentials` parameters for db cluster creation
    
    * databricks serve + john snow labs endpoints docs + typo fixes
    
    * databricks serve + john snow labs endpoints docs + typo fixes
    
    * updated tests
    
    * updated tests
    
    * fix block_until_deployed bug
    
    * fix block_until_deployed bug
    
    * support for GPU and nlu.predict params in endpoints
    
    * bump versions
    
    * Docs update
    
    * update notebooks
    
    * endpoint test job generator
    
    * improved list_db_runtime_versions
    
    * multi cluster testing endpoints
    
    * Support for submitting notebook to databricks and various utils & refactor to databricks utils
    
    * db test refactor into  db_info/endpoint/hdfs/submit tests
    
    * get_or_create_test_cluster() and varous more db testing utils, pytest.ini and non-verbose parameterization of tests
    
    * add docs for notebook execution
    
    * bump enterprise nlp version
    
    * Update EMR initialization script (#661)
    
    * update release notes
    
    * update version
    
    * Feature/513/parameterized db submit (#684)
    
    * support for parameterization of NB sumit runs in run_in_databricks()
    
    * tests for parameterization of NB sumit runs in run_in_databricks()
    
    * tests for parameterization of NB sumit runs in run_in_databricks()
    
    * Docs & example notebook for parameterized run_in_databricks
    
    * Feature/513/list of additional db installs (#689)
    
    * support for parameterization of NB sumit runs in run_in_databricks()
    
    * tests for parameterization of NB sumit runs in run_in_databricks()
    
    * tests for parameterization of NB sumit runs in run_in_databricks()
    
    * Docs & example notebook for parameterized run_in_databricks
    
    * Docs & example notebook for parameterized run_in_databricks
    
    * updated noteoboks
    
    * support for extra_pip_installs and pandas==1.5.3 fix on new clusters
    
    * refactor db cluster creation to install_to_databricks and add deprecation warning to nlp.install() for db clusters
    
    * db install  tests with custom libs
    
    * Fix update existing databricks cluster (#680)
    
    ---------
    
    Co-authored-by: Kshitiz <kshitiz.shakya22@gmail.com>

--- tests/databricks/submit_tests.py
+++ tests/databricks/submit_tests.py
@@ -101,3 +101,48 @@ def test_submit_notebook_task_to_databricks(creds, node_type):
             dst_path=dst_path,
         )
     )
+
+
+@db_cloud_node_params
+def test_submit_notebook_parameterized_task_to_databricks(creds, node_type):
+    cluster_id = get_or_create_test_cluster(creds, node_type, 0)
+    lic, host, token = creds
+
+    nb_path = "parameterized_nb_example.ipynb"
+    dst_path = "/Users/christian@johnsnowlabs.com/test.ipynb"
+    assert_job_suc(
+        nlp.run_in_databricks(
+            nb_path,
+            databricks_cluster_id=cluster_id,
+            databricks_host=host,
+            databricks_token=token,
+            run_name="Notebook Test",
+            dst_path=dst_path,
+            parameters={"input_text": "How are you today", "model_name": "sentiment"},
+        )
+    )
+
+
+@db_cloud_node_params
+def test_submit_script_parameterized_task_to_databricks(creds, node_type):
+    cluster_id = get_or_create_test_cluster(creds, node_type, 0)
+
+    script = """
+import sys
+print(f"First argument: {sys.argv[1]}, Second argument: {sys.argv[2]}")
+    """
+
+    lic, host, token = creds
+
+    arg1 = "My first arg"
+    arg2 = "My second arg"
+    assert_job_suc(
+        nlp.run_in_databricks(
+            script,
+            databricks_cluster_id=cluster_id,
+            databricks_host=host,
+            databricks_token=token,
+            run_name="Notebook Test",
+            parameters=[arg1, arg2],
+        )
+    )
