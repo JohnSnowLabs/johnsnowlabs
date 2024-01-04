33a3a0e7df56921bbc6db189fb2d88ec8df0a45a
Christian Kasim Loan <christian.kasim.loan@gmail.com>
2024-01-04 05:39:29 +0100

    bump OCR and update release notes

--- johnsnowlabs/settings.py
+++ johnsnowlabs/settings.py
@@ -24,8 +24,8 @@ raw_version_nlp_display = "4.1"
 raw_version_medical = "5.2.0"
 raw_version_secret_medical = "5.2.0"
 
-raw_version_secret_ocr = "5.1.0"
-raw_version_ocr = "5.1.0"
+raw_version_secret_ocr = "5.1.2"
+raw_version_ocr = "5.1.2"
 
 raw_version_pydantic = "1.10.11"
 
