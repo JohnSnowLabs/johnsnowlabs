6d916d8a5c0378c0833f3bf92428a5a7a19cbd50
Christian Kasim Loan <christian.kasim.loan@gmail.com>
2024-03-05 01:42:00 +0100

    refactored tests

--- tests/docker_tests.py
+++ tests/docker_tests.py
@@ -13,19 +13,138 @@ from johnsnowlabs.auto_install.docker.work_utils import (
 )
 from johnsnowlabs.utils.py_process import run_cmd_and_check_succ
 
-from johnsnowlabs import nlp
 
 # https://github.com/JohnSnowLabs/nlp-server/blob/main/nlp_server/routers/licenses.py
 
+def check_local_endpoint_health(port):
 
+
+    url = f"http://localhost:{port}/predict_batch"
+
+    params = {
+        "text": ["Your text that you want to predict with the model goes here",
+                 'It can also be a list of strings'],
+    }
+    response = requests.post(url, params=params, headers={"accept": "application/json"})
+    print('BATCH PREDICTION UNPARAMETERIZED:')
+    print(response.json())
+
+
+
+
+    params = {
+        "text": "Your text that you want to predict with the model goes here",
+    }
+    url = f"http://localhost:{port}/predict"
+    response = requests.get(url, params=params, headers={"accept": "application/json"})
+    print('STRING PREDICTION UNPARAMETERIZED:')
+    print(response.json())
+
+
+
+    params = {
+        "text": "Your text that you want to predict with the model goes here",
+        "output_level": "document",
+        "positions": "false",
+        "metadata": "false",
+        "drop_irrelevant_cols": "false",
+        "get_embeddings": "false",
+        "keep_stranger_features": "true",
+    }
+    response = requests.get(url, params=params, headers={"accept": "application/json"})
+    print('STRING PREDICTION PARAMETERIZED:')
+    print(response.json())
+
+
+
+
+
+
+def check_build_serve_query(nlu_ref, port, json_license_path=None):
+    # build if missing
+    container_name = f"{nlu_ref}_container"
+    image_name = f"{nlu_ref}_img"
+
+    build_image(
+        nlu_ref,
+        f"{nlu_ref}_img",
+        rebuild=True,
+        use_cache=False,
+        json_license_path=json_license_path
+    )
+    # Serve container, destroy if already running
+    serve_container(
+        destroy_container=True,
+        image_name=image_name,
+        container_name=container_name,
+        host_port=port,
+    )
+
+    # Wait for spark session start and model loading
+    time.sleep(30)
+    check_local_endpoint_health(port)
+
+    # Print container logs
+
+
+    run_cmd_and_check_succ(
+        f"docker logs {container_name}",
+        shell=True,
+        raise_on_fail=False,
+        use_code=True,
+        log=True,
+    )
+    # _destroy_container(container_name)
+    # _destroy_image(image_name)
 def test_docker():
     print(is_docker_installed())
 
 
 def test_build_image():
-    build_image(destroy_image=True, use_cache=False, preloaded_model="tokenize")
-    build_image(destroy_image=True, use_cache=True, preloaded_model="tokenize")
+    build_image(rebuild=True, use_cache=False, preloaded_model="tokenize")
+    build_image(rebuild=True, use_cache=True, preloaded_model="tokenize")
     _destroy_image()
+
+
+def test_serve_container_medical():
+    nlu_ref = "en.med_ner.cancer_genetics.pipeline"
+    port = 8549
+    check_build_serve_query(nlu_ref, port)
+
+
+def test_serve_container_ocr():
+    nlu_ref = "img2text"
+    port = 8549
+    container_name = f"{nlu_ref}_container"
+    image_name = f"{nlu_ref}_img"
+
+    build_image(
+        nlu_ref,
+        f"{nlu_ref}_img",
+        rebuild=True,
+        use_cache=False,
+        browser_login=True,
+        visual=True
+    )
+    # Serve container, destroy if already running
+    serve_container(
+        destroy_container=True,
+        image_name=image_name,
+        container_name=container_name,
+        host_port=port,
+    )
+def test_serve_container_open_source():
+    nlu_ref = "tokenize"
+    port = 8548
+    check_build_serve_query(nlu_ref, port)
+
+
+def test_query_server():
+    port = 8548 # tokenizerz
+    port = 8549 # en.med_ner.cancer_genetics.pipeline
+    # Send request to the container
+    check_local_endpoint_health(port)
+
 
 
 def test_run_container():
@@ -40,58 +159,3 @@ def test_run_container():
     )
     _destroy_container()
 
-
-def test_deploy_ecr():
-    build_image("tokenize", destroy_image=False, use_cache=True)
-
-
-def test_serve_container():
-    # build if missing
-    nlu_ref = "tokenize"
-    container_name = f"{nlu_ref}_container"
-    image_name = f"{nlu_ref}_img"
-    port = 8548
-
-    build_image(
-        nlu_ref,
-        f"{nlu_ref}_img",
-        destroy_image=True,
-        use_cache=False,
-    )
-    # Serve container, destroy if already running
-    serve_container(
-        destroy_container=True,
-        image_name=image_name,
-        container_name=container_name,
-        host_port=port,
-    )
-
-    # Wait for spark session start and model loading
-    time.sleep(30)
-
-    # Send request to the container
-    url = f"http://localhost:{port}/predict"
-    params = {
-        "text": "Your text that you want to predict with the model goes here",
-        "output_level": "document",
-        "positions": "false",
-        "metadata": "false",
-        "drop_irrelevant_cols": "false",
-        "get_embeddings": "false",
-        "keep_stranger_features": "true",
-    }
-    headers = {"accept": "application/json"}
-
-    response = requests.get(url, params=params, headers=headers)
-    print(response.json())
-
-    # Print container logs
-    run_cmd_and_check_succ(
-        f"docker logs {container_name}",
-        shell=True,
-        raise_on_fail=False,
-        use_code=True,
-        log=True,
-    )
-    # _destroy_container(container_name)
-    _destroy_image(image_name)
