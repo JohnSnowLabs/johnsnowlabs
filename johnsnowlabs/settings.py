1a03fb3c95129aa3b4a4dd3a173801eb2ab4112b
Christian Kasim Loan <christian.kasim.loan@gmail.com>
2024-08-27 00:48:22 +0200

    Release/544 (#1417)
    * Fix is_running_in_databricks function (#1222)
    
    * init
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * update
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    ---------
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    * bump nlu version
    
    * bumped sparknlp and healthcare to 540
    
    * added new healthcare annotations
    
    * remove VectorDB
    
    * Bump version
    
    * bump versions
    
    * add missing spark conf
    
    * bump versions
    
    * fix docker files missing in build
    
    * bump version
    
    * bump spark-nlp and spark_nlp_jsl versions to 541
    
    * add spark_nlp_jsl modules to jsl
    
    * bump version
    
    ---------
    
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    Co-authored-by: WeichenXu <weichen.xu@databricks.com>
    Co-authored-by: Mehmet Butgul <mehmetbutgulmail@gmail.com>

--- johnsnowlabs/settings.py
+++ johnsnowlabs/settings.py
@@ -10,7 +10,7 @@ from johnsnowlabs.utils.env_utils import (
 
 # These versions are used for auto-installs and version  checks
 
-raw_version_jsl_lib = "5.4.3"
+raw_version_jsl_lib = "5.4.4"
 
 raw_version_nlp = "5.4.1"
 
