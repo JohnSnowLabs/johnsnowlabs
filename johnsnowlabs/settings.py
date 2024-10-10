088a59395f424c9ead98de7483245781a271a82d
Mehmet Butgul <mehmetbutgulmail@gmail.com>
2024-10-10 18:33:23 +0300

    implemented spark-nlp-jsl==550 requirements

--- johnsnowlabs/settings.py
+++ johnsnowlabs/settings.py
@@ -12,7 +12,7 @@ from johnsnowlabs.utils.env_utils import (
 
 raw_version_jsl_lib = "5.4.5"
 
-raw_version_nlp = "5.4.1"
+raw_version_nlp = "5.5.0"
 
 raw_version_nlu = "5.4.1"
 
@@ -20,8 +20,8 @@ raw_version_nlu = "5.4.1"
 raw_version_pyspark = "3.4.0"
 raw_version_nlp_display = "5.0"
 
-raw_version_medical = "5.4.1"
-raw_version_secret_medical = "5.4.1"
+raw_version_medical = "5.5.0"
+raw_version_secret_medical = "5.5.0"
 
 raw_version_secret_ocr = "5.4.1"
 raw_version_ocr = "5.4.1"
