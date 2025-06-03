dd30279095159762c307cef5bf2e36b6222a81a8
Christian Kasim Loan <christian.kasim.loan@gmail.com>
2025-06-03 09:36:50 +0200

    Release/601 (#1824)
    * Bump versions and update docs
    
    * add AnnotationConverter to finance/legal/medical

--- johnsnowlabs/medical.py
+++ johnsnowlabs/medical.py
@@ -104,7 +104,8 @@ try:
             PretrainedZeroShotNER,
             StructuredJsonConverter,
             BertForAssertionClassification,
-            FhirDeIdentification
+            FhirDeIdentification,
+            AnnotationConverter
         )
         from sparknlp_jsl.structured_deidentification import StructuredDeidentification
         from sparknlp_jsl.text_to_documents_columns import TextToDocumentsColumns
